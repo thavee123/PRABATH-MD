64e08cdad283a25e9f8581c1c63e9d865d0b88bf
Thaveesha akalpa <bihandubihandu61@gmail.com>
2024-07-27 22:09:44 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,5 +1,5 @@
-const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
+const GITHUB_AUTH_TOKEN = "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"
+const BOT_NUMBER = "94773913394"
 const SESSION_ID = ""
 
 
